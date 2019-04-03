ce0a7e14828da829456d130bac22ef32e0f4a67a
Martin1442 <Martin1442@users.noreply.github.com>
2019-04-03 11:34:55 +0200

    Class04 - Exercise

---
+++ Class04-Exercise/PcConfigurator/Classes/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using CSharpAdvanced_Class4.Interfaces;
+
+namespace CSharpAdvanced_Class4
+{
+    public static class Extensions
+    {
+        public static string WithCurrency(this double value, string currency)
+        {
+            return $"{value:0,0.00}{currency}";
+        }
+
+        public static string PriceWithCurrency (this IPrice priced, string currency)
+        {
+            var price = priced.GetTotalPrice().WithCurrency(currency);
+            return price;
+        }
+    }
+}
