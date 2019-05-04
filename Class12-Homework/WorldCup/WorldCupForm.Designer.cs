8178d1b98234528933e5881bf2617935beed97c9
Martin1442 <Martin1442@users.noreply.github.com>
2019-05-04 11:24:46 +0200

    Homework -Class12

---
+++ Class12-Homework/WorldCup/WorldCupForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WorldCup
+{
+    partial class WorldCupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxTeams = new System.Windows.Forms.ComboBox();
+            this.lblSelectedTeam = new System.Windows.Forms.Label();
+            this.pboxBadge = new System.Windows.Forms.PictureBox();
+            this.pboxTeam = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxBadge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTeam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxTeams
+            // 
+            this.cbxTeams.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxTeams.FormattingEnabled = true;
+            this.cbxTeams.Location = new System.Drawing.Point(24, 15);
+            this.cbxTeams.Margin = new System.Windows.Forms.Padding(6);
+            this.cbxTeams.Name = "cbxTeams";
+            this.cbxTeams.Size = new System.Drawing.Size(238, 38);
+            this.cbxTeams.TabIndex = 0;
+            this.cbxTeams.SelectedIndexChanged += new System.EventHandler(this.cbxTeams_SelectedIndexChanged);
+            // 
+            // lblSelectedTeam
+            // 
+            this.lblSelectedTeam.AutoSize = true;
+            this.lblSelectedTeam.Location = new System.Drawing.Point(320, 15);
+            this.lblSelectedTeam.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblSelectedTeam.Name = "lblSelectedTeam";
+            this.lblSelectedTeam.Size = new System.Drawing.Size(0, 31);
+            this.lblSelectedTeam.TabIndex = 1;
+            // 
+            // pboxBadge
+            // 
+            this.pboxBadge.Location = new System.Drawing.Point(24, 60);
+            this.pboxBadge.Name = "pboxBadge";
+            this.pboxBadge.Size = new System.Drawing.Size(296, 317);
+            this.pboxBadge.TabIndex = 2;
+            this.pboxBadge.TabStop = false;
+            // 
+            // pboxTeam
+            // 
+            this.pboxTeam.Location = new System.Drawing.Point(326, 60);
+            this.pboxTeam.Name = "pboxTeam";
+            this.pboxTeam.Size = new System.Drawing.Size(472, 317);
+            this.pboxTeam.TabIndex = 3;
+            this.pboxTeam.TabStop = false;
+            // 
+            // WorldCupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 889);
+            this.Controls.Add(this.pboxTeam);
+            this.Controls.Add(this.pboxBadge);
+            this.Controls.Add(this.lblSelectedTeam);
+            this.Controls.Add(this.cbxTeams);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximumSize = new System.Drawing.Size(1084, 936);
+            this.Name = "WorldCupForm";
+            this.Text = "World Cup Images";
+            this.Load += new System.EventHandler(this.WorldCupForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pboxBadge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTeam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxTeams;
+        private System.Windows.Forms.Label lblSelectedTeam;
+        private System.Windows.Forms.PictureBox pboxBadge;
+        private System.Windows.Forms.PictureBox pboxTeam;
+    }
+}
+
---
+++ Class12-Homework/WorldCup/WorldCupForm.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace WorldCup
+{
+    public partial class WorldCupForm : Form
+    {
+        private string selectedTeam;
+
+        public WorldCupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void WorldCupForm_Load(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            string teamsString = wc.DownloadString("https://us-central1-sedc-world-cup.cloudfunctions.net/webApi/team-list");
+            var teams = JsonConvert.DeserializeObject<string[]>(teamsString);
+            cbxTeams.DataSource = teams;
+        }
+
+        private async void cbxTeams_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedTeam = cbxTeams.SelectedItem as string;
+            lblSelectedTeam.Text = $"You selected {selectedTeam}";
+            WebClient wc = new WebClient();
+            Uri uri = new Uri($"https://us-central1-sedc-world-cup.cloudfunctions.net/webApi/team/{selectedTeam}");
+
+            string teamString = await wc.DownloadStringTaskAsync(uri);
+            var team = JsonConvert.DeserializeObject<Team>(teamString);
+
+            
+            var badgeImageTask = new WebClient().DownloadDataTaskAsync(team.BadgeImageUrl);
+            var teamImageTask = new WebClient().DownloadDataTaskAsync(team.TeamImageUrl);
+
+            var results = await Task.WhenAll(badgeImageTask, teamImageTask);
+
+            using (var ms = new MemoryStream(results[0]))
+            {
+                pboxBadge.Image = Image.FromStream(ms);
+            }
+
+            using (var ms = new MemoryStream(results[1]))
+            {
+                pboxTeam.Image = Image.FromStream(ms);
+            }
+
+            foreach (Control pic in Controls)
+            {
+                var picture = pic as PictureBox;
+                if (picture != null && picture.Name == "Player")
+                {
+                    Controls.Remove(picture);
+                    picture.Dispose();
+                }
+            }
+
+            foreach(Control lbl in Controls)
+            {
+                var label = lbl as Label;
+                if (label != null)
+                {
+                    Controls.Remove(label);
+                    label.Dispose();
+                }
+            }
+
+
+            var x = 24;
+            var y = 435;
+            var picturesInLine = 0;
+
+            AutoScroll = false;
+            foreach (var player in team.Players)
+            {
+                if (string.IsNullOrEmpty(player.ImageUrl))
+                {
+                    continue;
+                }
+                var playerImage = await wc.DownloadDataTaskAsync(player.ImageUrl);
+
+                if(picturesInLine >= 4)
+                {
+                    x = 24;
+                    y = y + 330;
+                    picturesInLine = 0;
+                }
+
+                var pboxPlayer = new PictureBox
+                {
+                    Name = "Player",
+                    Size = new Size(238, 284),
+                    Location = new Point(x, y),
+                };
+
+                var lblName = new Label
+                {
+                    Name = "lblName",
+                    Size = new Size(230, 30),
+                    Location = new Point(x, y + 300)
+                };
+                
+                using (var ms = new MemoryStream(playerImage))
+                {
+                    pboxPlayer.Image = Image.FromStream(ms);
+                    lblName.Text = $"Name: {player.Name}";
+                    this.Controls.Add(pboxPlayer);
+                    this.Controls.Add(lblName);
+                }
+                
+                picturesInLine += 1;
+                x = x + 260;
+            }
+            AutoScroll = true;
+            
+            //wc.DownloadStringAsync(uri);
+            //wc.DownloadStringCompleted += Wc_DownloadStringCompleted;
+        }
+        
+
+        //private async void cbxTeams_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    selectedTeam = cbxTeams.SelectedItem as string;
+        //    lblSelectedTeam.Text = $"You selected {selectedTeam}";
+        //    WebClient wc = new WebClient();
+        //    Uri uri = new Uri($"https://us-central1-sedc-world-cup.cloudfunctions.net/webApi/team/{selectedTeam}");
+
+        //    string teamString = await wc.DownloadStringTaskAsync(uri);
+        //    var team = JsonConvert.DeserializeObject<Team>(teamString);
+
+        //    var badgeImage = await wc.DownloadDataTaskAsync(team.BadgeImageUrl);
+        //    using (var ms = new MemoryStream(badgeImage))
+        //    {
+        //        pboxBadge.Image = Image.FromStream(ms);
+        //    }
+
+        //    var teamImage = await wc.DownloadDataTaskAsync(team.TeamImageUrl);
+        //    using (var ms = new MemoryStream(teamImage))
+        //    {
+        //        pboxTeam.Image = Image.FromStream(ms);
+        //    }
+
+        //    foreach (var player in team.Players)
+        //    {
+        //        if (string.IsNullOrEmpty(player.ImageUrl))
+        //        {
+        //            continue;
+        //        }
+        //        var playerImage = await wc.DownloadDataTaskAsync(player.ImageUrl);
+        //        using (var ms = new MemoryStream(playerImage))
+        //        {
+        //            pboxPlayer.Image = Image.FromStream(ms);
+        //        }
+        //    }
+        //    //wc.DownloadStringAsync(uri);
+        //    //wc.DownloadStringCompleted += Wc_DownloadStringCompleted;
+        //}
+
+        //private void Wc_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        //{
+        //    var result = JsonConvert.DeserializeObject<Team>(e.Result);
+        //    WebClient wc = new WebClient();
+        //    Uri uri = new Uri(result.BadgeImageUrl);
+        //    wc.DownloadDataAsync(uri);
+        //    wc.DownloadDataCompleted += Wc_DownloadDataCompleted;
+        //    uri = new Uri(result.TeamImageUrl);
+        //    WebClient wc2 = new WebClient();
+        //    wc2.DownloadDataAsync(uri);
+        //    wc2.DownloadDataCompleted += Wc_DownloadDataCompleted1;
+        //}
+
+        //private void Wc_DownloadDataCompleted1(object sender, DownloadDataCompletedEventArgs e)
+        //{
+        //    using (var ms = new MemoryStream(e.Result))
+        //    {
+        //        pboxTeam.Image = Image.FromStream(ms);
+        //    }
+        //}
+
+        //private void Wc_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        //{
+        //    using (var ms = new MemoryStream(e.Result))
+        //    {
+        //        pboxBadge.Image = Image.FromStream(ms);
+        //    }
+        //}
+    }
+}
